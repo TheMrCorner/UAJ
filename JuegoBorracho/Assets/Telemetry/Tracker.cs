894331829e1a0d12efff1db7ca639ae7c7c6491c
Nope <leonorcu@ucm.es>
2020-04-18 13:24:27 +0200

    cola

--- JuegoBorracho/Assets/Telemetry/Tracker.cs
+++ JuegoBorracho/Assets/Telemetry/Tracker.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 
 
+
 public class Tracker
-{
+{     string FILE_NAME = "Telemetry";
 
     // -------------------- VARIABLES -------------------- //
     private static Tracker instance;
@@ -11,19 +12,35 @@ public class Tracker
 
 
     // -------------------- FUNCIONES -------------------- //
-    public static Tracker Instance
-    {
-        get
-        {
-            if (instance == null)
-                instance = new Tracker();
-
-            return instance;
-        }
+    public static Tracker Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new Tracker();
+            return instance;
+        }
     }
-
-    public void AddEvent(ref TrackerEvent trackerEvent)
-    {
-        _eventQueue.Enqueue(trackerEvent);
+    // Adds an event to the event queue
+    public void AddEvent(ref TrackerEvent trackerEvent)
+    {
+        _eventQueue.Enqueue(trackerEvent);
+    }    // Dumps the set number of events to json     // If numEvents == -1, dumps all the events left in the queue    public void DumpEventsToJson(int numEvents = -1)
+    {
+        if(_eventQueue.Count > 0)
+        {
+            if(numEvents > 0)
+            {
+                for(int i=0; i<numEvents && _eventQueue.Count>0; i++)
+                {
+                    TrackerEvent auxTE = _eventQueue.Dequeue();
+                    auxTE.DumpEventDataToJson();
+                }
+            }
+            else
+            {
+                
+            }
+        }
     }
 }

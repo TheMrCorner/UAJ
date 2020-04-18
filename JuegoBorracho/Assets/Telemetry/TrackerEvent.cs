aa35fd698e002254c4d3b1573361819957eef981
Nope <leonorcu@ucm.es>
2020-04-18 12:08:09 +0200

    tracker event abstracto

---
+++ JuegoBorracho/Assets/Telemetry/TrackerEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TrackerEvent
+{
+
+    float timeStamp;
+    TrackerEvent()
+    {
+        timeStamp = Time.time;
+    }
+
+    public abstract void FillEventData();
+    public abstract void DumpEventDataToJson();
+
+}

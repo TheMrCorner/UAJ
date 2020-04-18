0d290fd3604a76bc06378c552caf71831aa21e31
Nope <leonorcu@ucm.es>
2020-04-18 12:59:17 +0200

    tracker manager con cola de eventos

--- JuegoBorracho/Assets/Telemetry/Tracker.cs
+++ JuegoBorracho/Assets/Telemetry/Tracker.cs
@@ -1,23 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 
+
 public class Tracker
-{
-
-    // -------------------- VARIABLES -------------------- //
-    private static Tracker instance;
-
-
-
-    // -------------------- FUNCIONES -------------------- //
-    public static Tracker Instance
-    {
-        get
-        {
-            if (instance == null)
-                instance = new Tracker();
-
-            return instance;
-        }
-    }
+{
+
+    // -------------------- VARIABLES -------------------- //
+    private static Tracker instance;
+    private static Queue<TrackerEvent> _eventQueue;
+
+
+    // -------------------- FUNCIONES -------------------- //
+    public static Tracker Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new Tracker();
+
+            return instance;
+        }
+    }
+
+    public void AddEvent(ref TrackerEvent trackerEvent)
+    {
+        _eventQueue.Enqueue(trackerEvent);
+    }
 }

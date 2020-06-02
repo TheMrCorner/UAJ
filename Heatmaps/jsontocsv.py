387219f475ae6592916b8fd382b3215cfa0e16ec
Nope <leonorcu@ucm.es>
2020-06-02 13:04:42 +0200

    json to csv reading json

--- Heatmaps/jsontocsv.py
+++ Heatmaps/jsontocsv.py
@@ -1,16 +1,30 @@
-import pandas as pd
 import json
 import csv
 
+eventTypes =["ChangeState", "Damaged", "EnemyDeath", "PlayerDeath", "PlayerShoots", "InitLevel", "EndLevel"]
+filename = "Data/30052020/30052020_0.json"
 
-f = open(r'30052020_0.json')
-df = json.load(f)
+with open(filename, 'r', encoding='utf-8') as f:
+    jsonFile = json.load(f)
+
+jsonDic = {}
+num = range(len(jsonFile)) 
+for t in range(len(jsonFile)):
+    name = jsonFile[t]
+    name = list(name.keys())[0]
+    if name not in jsonDic:
+        jsonDic[name] = []
+    jsonFile[t][name]["Position"]
+    jsonDic[name].append(jsonFile[t][name]["Position"])
 
 
-cf = csv.writer(open("test.csv", "wb+"))
+co = open("test.csv", "w")
+cf = csv.writer(co)
 
-# Write CSV Header, If you dont need that, remove this line
-cf.writerow(["TimeSinceStart"])
+header = jsonDic.keys()
+values = list(jsonDic.values())
 
-for df in df:
-    cf.writerow([df["TimeSinceStart"]])
+cf.writerow(header)
+for v in values:      
+      cf.writerow(v)
+co.close()

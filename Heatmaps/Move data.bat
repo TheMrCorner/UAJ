d962ff20641810e32b3661797aa0de76a5b9ac9f
Raul <raulfe06@ucm.es>
2020-06-02 12:13:28 +0200

    Modificado un pelín el bash script

--- Heatmaps/Move data.bat
+++ Heatmaps/Move data.bat
@@ -1,3 +1,2 @@
-rem xcopy ..\JuegoBorracho\Assets\Telemetry\Results\* Data
-
+rem Copia los datos obtenidos desde Unity a la carpeta correspondiente para ser usados según convenga
 xcopy ..\JuegoBorracho\Assets\Telemetry\Results Data /E /EXCLUDE:Data\.exclude.txt

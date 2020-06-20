f74c47ea1f141a27764da256b462aed38172acc2
Alejandro Marín <alpermar98@gmail.com>
2020-06-20 11:20:21 +0200

    Nuevos eventos añadidos: Dash y Espada

---
+++ JuegoBorracho/Assets/Telemetry/TrackerEventTypes/TEventDash.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEventDash : TrackerEvent
+{
+    public TEventDash(float timeStamp, Position2D pos) : base(timeStamp, pos, "PlayerDash")
+    {
+
+    }
+}
---
+++ JuegoBorracho/Assets/Telemetry/TrackerEventTypes/TEventSword.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEventSword : TrackerEvent
+{
+    public TEventSword(float timeStamp, Position2D pos) : base(timeStamp, pos, "PlayerSword")
+    {
+
+    }
+}
